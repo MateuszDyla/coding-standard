d9f96827bf47c01127bc4be3a755a325242fad67
Mateusz Dyla <88793508+MateuszDyla@users.noreply.github.com>
2021-11-16 11:20:28 +0100

    Create ecs.php

---
+++ ecs.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use BitBag\CodingStandards\Fixer\FinalClassInEntitiesOrRepositoriesFixer;
+use BitBag\CodingStandards\Fixer\AboveTwoArgumentsMultilineFixer;
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use PhpCsFixer\Fixer\ControlStructure\YodaStyleFixer;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import('vendor/sylius-labs/coding-standard/ecs.php');
+    $services = $containerConfigurator->services();
+    $services->set(ArraySyntaxFixer::class)
+        ->call('configure', [[
+            'syntax' => 'short',
+        ]]);
+    $services->set(YodaStyleFixer::class);
+
+    $services->set(FinalClassInEntitiesOrRepositoriesFixer::class);
+  
+    $services->set(AboveTwoArgumentsMultilineFixer::class);
+
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PATHS, [
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+};
